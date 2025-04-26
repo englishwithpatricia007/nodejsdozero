2fc03f2f9f1425d52c3233ac3f9c08f640a8fca0
Patricia <englishwithpatricia007@gmail.com>
2025-04-26 12:01:56 -0300

    criado servidor HTTP nativo

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import { createServer } from 'node:http'
 
-const server = createServer(() => {  
-    console.log('Server is running...')
-
+const server = createServer((request, response) => {  
+    //console.log('Server is running...')
+    response.write("Sayonara!")
+    return response.end()
 })
 server.listen(3333, () => {
     console.log('Server is listening on port 3333')
