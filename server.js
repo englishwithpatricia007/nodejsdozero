adf1dc97acd1f73df6ee7be0f08a4f0add3665c3
Patricia <englishwithpatricia007@gmail.com>
2025-04-26 12:17:09 -0300

    Criando servidor com Fastify

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createServer } from 'node:http'
+/*import { createServer } from 'node:http'
 
 const server = createServer((request, response) => {  
     //console.log('Server is running...')
@@ -7,4 +7,23 @@ const server = createServer((request, response) => {
 })
 server.listen(3333, () => {
     console.log('Server is listening on port 3333')
+})*/
+import { fastify } from 'fastify'
+const server = fastify()
+
+server.get('/', (request, reply) => {
+    return 'Hello World!'
+})
+
+server.get('/users', (request, reply) => {
+    return [{ name: 'John Doe', age: 30 }, { name: 'Jane Doe', age: 25 }]
+})
+
+server.get('/users/:id', (request, reply) => {
+    const { id } = request.params
+    return { id, name: 'John Doe', age: 30 }
+})
+
+server.listen({
+    port: 3333,
 })
